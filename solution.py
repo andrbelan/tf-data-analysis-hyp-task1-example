75f56dfc704ffe4899ced0c1dafecc0b245fc1ca
andrbelan <130162109+andrbelan@users.noreply.github.com>
2023-04-13 20:21:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import scipy as sp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 412238846 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    table = np.array([[x_success, x_cnt - x_success], [y_success, y_cnt - y_success]])
+    pv=st.chi2_contingency(table)[1]/2 
+    if pv > 0.09:
+      return False
+    else:
+      return True
